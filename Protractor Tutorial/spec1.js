ef65c6bf937c8bf5ca516498a360205f7e886d72
RamshaWarsi <40998779+RamshaWarsi@users.noreply.github.com>
2021-05-26 17:20:09 +0500

    this is first comit where chrome browser will be launched.

---
+++ Protractor Tutorial/spec1.js
@@ -0,0 +1,12 @@
+describe('Protractor Tutoriall',function()
+//Decribe has two psrsmeters. First Test suite name
+		//Second - Function- It has all the it blocks
+	
+	{
+	it('First JS Website', function()
+			{
+//it has two parameters. First is the name of testcase.
+//second is the actual testcases codes in JS.
+		browser.get('https://angularjs.org/');
+			})
+	})
