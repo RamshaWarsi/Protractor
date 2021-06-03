9760a14e38e44306ed7a74215eb3f10f1025c9e2
RamshaWarsi <40998779+RamshaWarsi@users.noreply.github.com>
2021-06-03 17:39:32 +0500

    JavaScript Basics. Run as node.js application.

--- Protractor Tutorial/Basics.js
+++ Protractor Tutorial/Basics.js
@@ -1,3 +1,9 @@
 /**
  * 
  */
+//console.log ("Hello World") // to print anything in console. 
+//Js is dynamic Type language. data type to be termed as variable.
+var a = 5;
+var b ="Hello World"; 
+console.log (a);
+console.log (b);
