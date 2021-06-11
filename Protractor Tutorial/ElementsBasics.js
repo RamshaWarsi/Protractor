f1ac0103d4861ecd503ef6be650592a2e97fba7c
Ramsha Warsi <rwa@ciklum.com>
2021-06-11 11:56:51 +0500

    Elements basics are done in this. to send keys to the respective calculator

--- Protractor Tutorial/ElementsBasics.js
+++ Protractor Tutorial/ElementsBasics.js
@@ -1,15 +1,11 @@
-/**
- * 
- */
 describe('Protractor Ellements Basics',function()
 
-	
 	{
 	it('locators', function()
 			{
 browser.get("http://juliemr.github.io/protractor-demo/");
-element(by.model("First")).sendKeys("3");
-element(by.model(second).sendkeys("5"))
-		})
-	})
-	
+
+    });
+
+});
+
