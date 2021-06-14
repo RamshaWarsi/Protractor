f013fe762d90706017f794fd431d028beda8b07b
Ramsha Warsi <rwa@ciklum.com>
2021-06-14 18:33:27 +0500

    The calculator website is not launched. Using https://angularjs.org/ for automating.

--- Protractor Tutorial/ElementsBasics.js
+++ Protractor Tutorial/ElementsBasics.js
@@ -1,11 +1,14 @@
-describe('Protractor Ellements Basics',function()
+describe('Protractor Elements Basics',function()
 
 	{
 	it('locators', function()
 			{
-browser.get("http://juliemr.github.io/protractor-demo/");
-
-    });
-
-});
-
+browser.get("https://angularjs.org/"); //Calculator site is not launched by angular.
+browser.sleep(200000);
+element(by.className("nav-link-inner")).sendKeys("8");
+//element(by.model("second")).sendKeys("10").then (function()
+//{
+//browser.sleep(10000);
+} )
+	}
+)
--- Protractor Tutorial/config.js
+++ Protractor Tutorial/config.js
@@ -1,4 +1,5 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['ElementsBasics.js'] //run webdriver again. 
+	
 };
